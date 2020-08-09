c6f75b0f1ff495701a46060e1935e3998f69c128
Chris <chris.eddy.dev@gmail.com>
2020-08-09 10:49:47 -0700

    Updates

---
+++ precache-manifest.1494fcca405a7afd36264428591bfd81.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ff7a91187136ababc3d",
+    "url": "build/2.9ff7a911.js"
+  },
+  {
+    "revision": "9aed087eeb350708897d68972edb4a26",
+    "url": "build/2.9ff7a911.js.LICENSE.txt"
+  },
+  {
+    "revision": "39a4c5011a8a6d39ba32",
+    "url": "build/bundle.39a4c501.js"
+  },
+  {
+    "revision": "ea2a04ed037d590abd02",
+    "url": "build/main.ea2a04ed.js"
+  },
+  {
+    "revision": "a5ecd8bd9f92345c01d2fe402afc56ae",
+    "url": "index.html"
+  },
+  {
+    "revision": "9ff7a91187136ababc3d",
+    "url": "static/css/2.246d11fe.chunk.css"
+  },
+  {
+    "revision": "ea2a04ed037d590abd02",
+    "url": "static/css/main.10022bcd.chunk.css"
+  },
+  {
+    "revision": "e128ac44faa84b2d59c10c016fad0778",
+    "url": "static/media/Mulish-Regular.e128ac44.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.ab7a52b80a81c9ad60619c32fc9e32f6.js"
+  "precache-manifest.1494fcca405a7afd36264428591bfd81.js"
 );
 
 self.addEventListener('message', (event) => {
