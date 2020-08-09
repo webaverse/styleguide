5a4c12c3f8305fd765319356ff93ca6714de3794
Chris <chris.eddy.dev@gmail.com>
2020-08-09 11:34:34 -0700

    Updates

---
+++ precache-manifest.92c73fc3ecca5e2978fbc3add41e217b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6cf7e9b4d80a079d2e1",
+    "url": "build/2.f6cf7e9b.js"
+  },
+  {
+    "revision": "3b131bf66fbf5bdb5043b2e42a20c198",
+    "url": "build/2.f6cf7e9b.js.LICENSE.txt"
+  },
+  {
+    "revision": "e46008162eaa4d8d14fe",
+    "url": "build/bundle.e4600816.js"
+  },
+  {
+    "revision": "e6c1e2a670133b093c11",
+    "url": "build/main.e6c1e2a6.js"
+  },
+  {
+    "revision": "e28c99c9c665cbb8c714d3c72c96fd9a",
+    "url": "index.html"
+  },
+  {
+    "revision": "f6cf7e9b4d80a079d2e1",
+    "url": "static/css/2.246d11fe.chunk.css"
+  },
+  {
+    "revision": "e6c1e2a670133b093c11",
+    "url": "static/css/main.466d1b52.chunk.css"
+  },
+  {
+    "revision": "e128ac44faa84b2d59c10c016fad0778",
+    "url": "static/media/Mulish-Regular.e128ac44.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.49c82926bad6e81bf6a24411485709a9.js"
+  "precache-manifest.92c73fc3ecca5e2978fbc3add41e217b.js"
 );
 
 self.addEventListener('message', (event) => {
