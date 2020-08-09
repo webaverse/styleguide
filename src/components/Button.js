d1d96607eb5249d020b4e4b6587997fa49444fe0
Chris <chris.eddy.dev@gmail.com>
2020-08-09 10:39:19 -0700

    skin Button to xrpackage style + add fonts to repo

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,9 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import 'antd/dist/antd.min.css'
 import { Button as AntButton } from 'antd';
+import './Button.scss';
+
+const styles = {
+    button: {
+        backgroundColor: '#42a5f5',
+        color: 'white',
+        border: 0,
+        borderRadius: 8
+    }
+}
 
 const Button = props => {
-    return <AntButton {...props}>{props.text}</AntButton>
+    return <AntButton {...props} style={styles.button}>{props.text}</AntButton>
 }
 
 Button.propTypes = {
